181a620b69e739f32c40b7f307eb89b60896e88c
Fankaren <18942405842@163.com>
2021-02-09 10:50:20 +0800

    second commit

--- functions/zhangsana/inline.js
+++ functions/zhangsana/inline.js
@@ -4,14 +4,14 @@ module.exports = async (ctx) => {
         return
     } else {
         const answer = []
-        const tocamos = [2,3,4,5,6,7,8,9,10]
-        tocamos.forEach(function(tocamos) {
+        const zhangsan = [2,3,4,5,6,7,8,9,10]
+        zhangsan.forEach(function(zhangsan) {
             answer.push({
-                id: tocamos,
-                title: tocamos+" ("+search+" entre "+tocamos+")",
+                id: zhangsan,
+                title: zhangsan+" ("+search+" entre "+zhangsan+")",
                 type: 'article',
                 input_message_content: {
-                    message_text: "Tocais cada uno a " + (Math.round(search/tocamos)*100)/100+" ("+search+" entre "+tocamos+")",
+                    message_text: "Tocais cada uno a " + (Math.round(search/zhangsan)*100)/100+" ("+search+" entre "+zhangsan+")",
                     parse_mode: 'HTML'
                 }
             })
